d1ff21e22eb9cd110c6914d68486baf71bcf044a
Kane Rogers <kane.m.rogers@gmail.com>
2020-11-20 00:27:13 +1100

    wip

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,6 @@ use winit::{
     window::{Window, WindowBuilder},
 };
 
-enum CurrentShader {
-    Boring,
-    Positional,
-}
-
 struct State {
     surface: wgpu::Surface,
     device: wgpu::Device,
@@ -21,8 +16,13 @@ struct State {
     size: winit::dpi::PhysicalSize<u32>,
     colour: wgpu::Color,
     render_pipeline: wgpu::RenderPipeline,
-    current_shader: CurrentShader,
     vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+    num_indicies: u32,
+    challenge_vertex_buffer: wgpu::Buffer,
+    challenge_index_buffer: wgpu::Buffer,
+    num_challenge_indicies: u32,
+    use_complex: bool,
 }
 
 #[repr(C)]
@@ -32,22 +32,62 @@ struct Vertex {
     color: [f32; 3],
 }
 
+impl Vertex {
+    fn desc<'a>() -> wgpu::VertexBufferDescriptor<'a> {
+        wgpu::VertexBufferDescriptor {
+            stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::InputStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttributeDescriptor {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float3,
+                },
+                wgpu::VertexAttributeDescriptor {
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float3,
+                },
+            ],
+        }
+    }
+}
+
 unsafe impl bytemuck::Pod for Vertex {}
 unsafe impl bytemuck::Zeroable for Vertex {}
 
 const VERTICIES: &[Vertex] = &[
     Vertex {
-        position: [0.0, 0.5, 0.0],
-        color: [1.0, 0.0, 0.0],
-    },
+        position: [-0.0868241, 0.49240386, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // A
     Vertex {
-        position: [-0.5, -0.5, 0.0],
-        color: [0.0, 1.0, 0.0],
-    },
+        position: [-0.49513406, 0.06958647, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // B
     Vertex {
-        position: [0.5, -0.5, 0.0],
-        color: [0.0, 0.0, 1.0],
-    },
+        position: [-0.21918549, -0.44939706, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // C
+    Vertex {
+        position: [0.35966998, -0.3473291, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // D
+    Vertex {
+        position: [0.44147372, 0.2347359, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // E
+    Vertex {
+        position: [0.54147372, 0.5347359, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // F
+];
+
+const INDICIES: &[u16] = &[
+    0, 1, 4, // A B E
+    1, 2, 4, // B C D
+    2, 3, 4, // C D E
+    0, 5, 4, // E F D
 ];
 
 impl State {
@@ -96,6 +136,44 @@ impl State {
             contents: bytemuck::cast_slice(VERTICIES),
             usage: wgpu::BufferUsage::VERTEX,
         });
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(INDICIES),
+            usage: wgpu::BufferUsage::INDEX,
+        });
+        let num_indicies = INDICIES.len() as u32;
+
+        let num_verticies = 512;
+        let angle = std::f32::consts::PI * 2.0 / num_verticies as f32;
+        let challenge_verts = (0..num_verticies)
+            .map(|i| {
+                let theta = angle * i as f32;
+                Vertex {
+                    position: [0.5 * theta.cos(), -0.5 * theta.sin(), 0.0],
+                    color: [(1.0 + theta.cos()) / 2.0, (1.0 + theta.sin()) / 2.0, 1.0],
+                }
+            })
+            .collect::<Vec<_>>();
+
+        let num_triangles = num_verticies - 2;
+        let challenge_indicies = (1u16..num_triangles + 1)
+            .into_iter()
+            .flat_map(|i| vec![i + 1, i, 0])
+            .collect::<Vec<_>>();
+        let num_challenge_indicies = challenge_indicies.len() as u32;
+
+        let challenge_vertex_buffer =
+            device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("Challenge Vertex Buffer"),
+                contents: bytemuck::cast_slice(&challenge_verts),
+                usage: wgpu::BufferUsage::VERTEX,
+            });
+
+        let challenge_index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Challenge Index Buffer"),
+            contents: bytemuck::cast_slice(&challenge_indicies),
+            usage: wgpu::BufferUsage::INDEX,
+        });
 
         Self {
             surface,
@@ -110,9 +188,14 @@ impl State {
                 b: 0.3,
                 a: 1.0,
             },
-            current_shader: CurrentShader::Boring,
             render_pipeline,
             vertex_buffer,
+            index_buffer,
+            num_indicies,
+            challenge_vertex_buffer,
+            challenge_index_buffer,
+            num_challenge_indicies,
+            use_complex: false,
         }
     }
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
@@ -124,20 +207,20 @@ impl State {
 
     fn input(&mut self, event: &WindowEvent) -> bool {
         match event {
-            WindowEvent::KeyboardInput { input, .. } => match input {
-                KeyboardInput {
-                    state: ElementState::Pressed,
-                    virtual_keycode: Some(VirtualKeyCode::Space),
-                    ..
-                } => {
-                    self.switch_pipelines();
-                }
-                _ => {}
-            },
-            _ => {}
+            WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state,
+                        virtual_keycode: Some(VirtualKeyCode::Space),
+                        ..
+                    },
+                ..
+            } => {
+                self.use_complex = *state == ElementState::Pressed;
+                true
+            }
+            _ => false,
         }
-
-        return false;
     }
 
     fn update(&mut self) {
@@ -170,44 +253,32 @@ impl State {
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
-            render_pass.draw(0..3, 0..1);
+            let data = if self.use_complex {
+                (
+                    &self.challenge_vertex_buffer,
+                    &self.challenge_index_buffer,
+                    self.num_challenge_indicies,
+                )
+            } else {
+                (&self.vertex_buffer, &self.index_buffer, self.num_indicies)
+            };
+            render_pass.set_vertex_buffer(0, data.0.slice(..));
+            render_pass.set_index_buffer(data.1.slice(..));
+            render_pass.draw_indexed(0..data.2, 0, 0..1);
         }
 
         self.queue.submit(once(encoder.finish()));
-    }
-
-    fn switch_pipelines(&mut self) {
-        let (vs_module, fs_module) = match self.current_shader {
-            CurrentShader::Boring => {
-                self.current_shader = CurrentShader::Positional;
-                let vs_module = self
-                    .device
-                    .create_shader_module(wgpu::include_spirv!("positional.vert.spv"));
-                let fs_module = self
-                    .device
-                    .create_shader_module(wgpu::include_spirv!("positional.frag.spv"));
-                (vs_module, fs_module)
-            }
-            CurrentShader::Positional => {
-                self.current_shader = CurrentShader::Boring;
-                let vs_module = self
-                    .device
-                    .create_shader_module(wgpu::include_spirv!("shader.vert.spv"));
-                let fs_module = self
-                    .device
-                    .create_shader_module(wgpu::include_spirv!("shader.frag.spv"));
-                (vs_module, fs_module)
-            }
-        };
-
-        self.render_pipeline = create_pipeline(&self.device, &self.sc_desc, vs_module, fs_module);
     }
 }
 
 fn main() {
     env_logger::init();
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let window = WindowBuilder::new()
+        .with_title("Learn wgpu")
+        .with_inner_size(winit::dpi::LogicalSize::new(1024.0, 1024.0))
+        .build(&event_loop)
+        .unwrap();
     let mut state = block_on(State::new(&window));
 
     event_loop.run(move |event, _, control_flow| match event {
@@ -286,7 +357,7 @@ fn create_pipeline(
         depth_stencil_state: None,
         vertex_state: wgpu::VertexStateDescriptor {
             index_format: wgpu::IndexFormat::Uint16,
-            vertex_buffers: &[],
+            vertex_buffers: &[Vertex::desc()],
         },
         sample_count: 1,
         sample_mask: !0,
