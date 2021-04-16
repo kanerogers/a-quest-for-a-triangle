22bb2027951bdfc8f5e427100c3a87be4fe5cfb7
Kane Rogers <kane.m.rogers@gmail.com>
2021-04-16 18:42:41 +1000

    wip

--- src/vulkan_wrapper.rs
+++ src/vulkan_wrapper.rs
@@ -573,50 +573,11 @@ fn create_swap_chain (
     entry: &Entry,
     physical_device: vk::PhysicalDevice,
     logical_device: &Device) -> (khr::Swapchain, vk::SwapchainKHR, vk::Format, vk::Extent2D) {
-    let surface_format = choose_swap_surface_format(swap_chain_support.surface_formats);
-    let present_mode = choose_swap_present_mode(swap_chain_support.present_modes);
-    let extent = choose_swap_extent(swap_chain_support.capabilities, window);
-
-    let image_count = swap_chain_support.capabilities.min_image_count + 1;
-    let image_count = if swap_chain_support.capabilities.max_image_count > 0 && image_count > swap_chain_support.capabilities.max_image_count {
-        swap_chain_support.capabilities.max_image_count
-    } else {
-        image_count
-    };
-
-    let indices = QueueFamilyIndices::find_queue_families(instance, physical_device, entry);
-    let indices_are_same = indices.are_same();
-    let image_sharing_mode = if indices_are_same { vk::SharingMode::EXCLUSIVE } else { vk::SharingMode::CONCURRENT };
-    let queue_family_indices = if indices_are_same { Vec::new() } else { vec![indices.graphics_family.unwrap(), indices.present_family.unwrap()] };
-
-    let create_info = vk::SwapchainCreateInfoKHR::builder()
-        .min_image_count(image_count)
-        .image_format(surface_format.format)
-        .image_color_space(surface_format.color_space)
-        .image_extent(extent)
-        .image_array_layers(1)
-        .image_sharing_mode(image_sharing_mode)
-        .queue_family_indices(&queue_family_indices)
-        .pre_transform(swap_chain_support.capabilities.current_transform)
-        .composite_alpha(vk::CompositeAlphaFlagsKHR::OPAQUE)
-        .present_mode(present_mode)
-        .clipped(true)
-        .old_swapchain(vk::SwapchainKHR::null())
-        .image_usage(vk::ImageUsageFlags::COLOR_ATTACHMENT);
-
-    let swapchain_ext = khr::Swapchain::new(instance, logical_device);
-    let swapchain = unsafe { swapchain_ext.create_swapchain(&create_info, None) }.expect("Unable to create Swapchain");
-    (swapchain_ext, swapchain, surface_format.format, extent)
+        todo!()
 }
 
-fn choose_swap_surface_format(formats: Vec<vk::SurfaceFormatKHR>) -> vk::SurfaceFormatKHR {
-    for available_format in &formats { 
-        if available_format.format == vk::Format::B8G8R8A8_SRGB && available_format.color_space == vk::ColorSpaceKHR::SRGB_NONLINEAR {
-            return *available_format
-        }
-    }
-
-    return *formats.first().unwrap();
+fn get_format() -> vk::Format {
+    todo!()
 }
 
 fn choose_swap_present_mode(available_present_modes: Vec<vk::PresentModeKHR>) -> vk::PresentModeKHR {
@@ -807,7 +768,7 @@ fn get_layer_names(entry: &Entry) -> Vec<*const i8> {
             "Unsupported layer: {:?}",
             layer
         );
-        validation_layers_raw.push(layer.as_ptr())
+        validation_layers_raw.push(layer.as_ptr() as *const i8)
     }
 
     return validation_layers_raw;
